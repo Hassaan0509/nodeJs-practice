cd5aab6f224445f9bf7f1dff3f236b9ffae3bddf
Hassaan Ahmad <2020CS678@student.uet.edu.pk>
2022-08-31 20:15:33 +0500

    Node practice uploaded

---
+++ Practice/os_module.js
@@ -0,0 +1,19 @@
+const os = require("os");
+
+//to get architecture
+console.log(os.arch());
+
+//to get free memory
+console.log(os.freemem());
+
+//to get total memory
+console.log(os.totalmem());
+
+//to get hostname
+console.log(os.hostname());
+
+//to get platform
+console.log(os.platform);
+
+//to get type
+console.log(os.type());
